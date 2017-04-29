8bff3f39484339fcc7b7af2c66b1a983386cbf05
Peter Rønsby <pr@activeforanimals.com>
2017-04-29 12:59:41 +0200

    AFA-105 Added publish/unpublish batch job and fixed import publish bug

--- src/Form/Group/GroupPublishForm.php
+++ src/Form/Group/GroupPublishForm.php
@@ -5,7 +5,7 @@ namespace Drupal\effective_activism\Form\Group;
 use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
-use Drupal\effective_activism\Helper\PublishHelper;
+use Drupal\effective_activism\Helper\Publish\Publisher;
 
 /**
  * Form controller for Group publish forms.
@@ -103,16 +103,28 @@ class GroupPublishForm extends ConfirmFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $entity = \Drupal::request()->get('group');
     if ($this->isPublished === TRUE) {
-      PublishHelper::unpublish($entity);
-      drupal_set_message(t('<em>@label</em> has been unpublished', [
-        '@label' => $entity->label(),
-      ]));
+      $publisher = new Publisher($entity);
+      $batch = [
+        'title' => t('Unpublishing...'),
+        'operations' => [[
+          'Drupal\effective_activism\Helper\Publish\BatchProcess::unpublish',
+          [$publisher],
+        ]],
+        'finished' => 'Drupal\effective_activism\Helper\Publish\BatchProcess::unpublished',
+      ];
+      batch_set($batch);
     }
     else {
-      PublishHelper::publish($entity);
-      drupal_set_message(t('<em>@label</em> has been published', [
-        '@label' => $entity->label(),
-      ]));
+      $publisher = new Publisher($entity);
+      $batch = [
+        'title' => t('Publishing...'),
+        'operations' => [[
+          'Drupal\effective_activism\Helper\Publish\BatchProcess::publish',
+          [$publisher],
+        ]],
+        'finished' => 'Drupal\effective_activism\Helper\Publish\BatchProcess::published',
+      ];
+      batch_set($batch);
     }
     // Redirect the user to the list controller when complete.
     $form_state->setRedirectUrl($this->getCancelUrl());
---
+++ src/Helper/Publish/BatchProcess.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\effective_activism\Helper\Publish;
+
+use Drupal;
+
+/**
+ * Processes batches of entity publish/unpublish actions.
+ */
+class BatchProcess {
+
+  /**
+   * Imports from a parser.
+   *
+   * @param Publisher $publisher
+   *   The publisher object to unpublish entities with.
+   * @param array $context
+   *   The context.
+   */
+  public static function unpublish(Publisher $publisher, array &$context) {
+    // Set inital batch values.
+    if (empty($context['sandbox'])) {
+      $context['sandbox']['progress'] = 0;
+    }
+    $context['message'] = t('Unpublishing items...');
+    foreach ($publisher->getNextBatch($context['sandbox']['progress']) as $item) {
+      $context['results'][] = $publisher->unpublish($item);
+      $context['sandbox']['progress']++;
+    }
+    // Inform batch about progess.
+    $context['finished'] = $context['sandbox']['progress'] / $publisher->getCount();
+  }
+
+  /**
+   * Publishes one or more entities.
+   *
+   * @param Publisher $publisher
+   *   The publisher object to publish entities with.
+   * @param array $context
+   *   The context.
+   */
+  public static function publish(Publisher $publisher, array &$context) {
+    // Set inital batch values.
+    if (empty($context['sandbox'])) {
+      $context['sandbox']['progress'] = 0;
+    }
+    $context['message'] = t('Publishing items...');
+    foreach ($publisher->getNextBatch($context['sandbox']['progress']) as $item) {
+      $context['results'][] = $publisher->publish($item);
+      $context['sandbox']['progress']++;
+    }
+    // Inform batch about progess.
+    $context['finished'] = $context['sandbox']['progress'] / $publisher->getCount();
+  }
+
+  /**
+   * Finishes a batch call.
+   *
+   * @param bool $success
+   *   Wether or not any fatal PHP errors were encountered.
+   * @param array $results
+   *   The result of the import.
+   * @param array $operations
+   *   The operations performed.
+   */
+  public static function unpublished($success, array $results, array $operations) {
+    if ($success) {
+      drupal_set_message(Drupal::translation()->formatPlural(
+        count($results),
+        'One item unpublished.',
+        '@count items unpublished.'
+      ));
+    }
+    else {
+      drupal_set_message(t('An error occured during unpublishing.'), 'error');
+    }
+  }
+
+  /**
+   * Finishes a batch call.
+   *
+   * @param bool $success
+   *   Wether or not any fatal PHP errors were encountered.
+   * @param array $results
+   *   The result of the import.
+   * @param array $operations
+   *   The operations performed.
+   */
+  public static function published($success, array $results, array $operations) {
+    if ($success) {
+      drupal_set_message(Drupal::translation()->formatPlural(
+        count($results),
+        'One item published.',
+        '@count items published.'
+      ));
+    }
+    else {
+      drupal_set_message(t('An error occured during publishing.'), 'error');
+    }
+  }
+}
