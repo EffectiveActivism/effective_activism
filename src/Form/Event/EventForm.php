223f893b063641690baf106dc37295d21b89b439
Peter Rønsby <pr@activeforanimals.com>
2018-02-02 20:09:11 +0200

    AFA-203 Added check to ensure Drupal date object

--- src/Form/Event/EventForm.php
+++ src/Form/Event/EventForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\effective_activism\Form\Event;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\effective_activism\Entity\Group;
@@ -118,7 +119,11 @@ class EventForm extends ContentEntityForm {
     // Event start date must be older or equal to end date.
     $start_date = $form_state->getValue('start_date')[0]['value'];
     $end_date = $form_state->getValue('end_date')[0]['value'];
-    if ($start_date->format('U') > $end_date->format('U')) {
+    if (
+      $start_date instanceof DrupalDateTime &&
+      $end_date instanceof DrupalDateTime &&
+      $start_date->format('U') > $end_date->format('U')
+    ) {
       $form_state->setErrorByName('end_date', $this->t('End date must be later than start date.'));
     }
   }
