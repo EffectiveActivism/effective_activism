19b07f4758fb019bbc72411ad3cc3f91ed73f10b
Peter Rønsby <pr@activeforanimals.com>
2018-07-22 20:41:34 +0200

    AFA-259 Added support for leaflet maps

---
+++ assets/js/leaflet.js
@@ -0,0 +1,44 @@
+/**
+ * @file
+ */
+
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.leaflet = {
+    attach: function (context, settings) {
+      var bounds = [];
+      var map = new L.Map(
+        'leaflet-map',
+        {
+          zoom: 9
+        }
+      );
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiYWN0aXZlZm9yYW5pbWFscyIsImEiOiJjamp1YXgzamowMzlqM3lxd3Q4YTVnbjhxIn0.1pBgnP88zkzKhqby_xK1RQ'
+      }).addTo(map);
+      drupalSettings.leaflet.forEach(function(element) {
+        // Add popup with element information.
+        var popup = L.popup();
+        if (element.description === null) {
+          popup.setContent('<a href="' + element.url + '"><h1>' + element.title + '</h1></a>');
+        }
+        else {
+          popup.setContent('<a href="' + element.url + '"><h1>' + element.title + '</h1><p>' + element.description + '</p></a>');
+        }
+        // Add marker for element.
+        L.marker([element.gps.latitude, element.gps.longitude], {
+          title: element.title,
+          alt: element.title,
+        }).bindPopup(popup).openPopup().addTo(map);
+        // Add coordinates to bounding box.
+        bounds.push([element.gps.latitude, element.gps.longitude]);
+      });
+      map.fitBounds(bounds);
+    },
+  };
+
+})(jQuery, Drupal, drupalSettings);
