d398d9bd30f83e7106c79b8db26d37699fb02155
Peter Rønsby <pr@activeforanimals.com>
2018-09-28 10:03:47 +0200

    AFA-227 Various fixes and added markercluster support

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -7,6 +7,12 @@
 
   Drupal.behaviors.leaflet = {
     attach: function (context, settings) {
+
+      if (drupalSettings.leaflet === null) {
+        // Reset map.
+        document.getElementById('ajax-map').innerHTML = '<div id="leaflet-map"">No data for the given filter and data type. Try changing values or create more events.</div>';
+        return;
+      }
       // Reset map.
       document.getElementById('ajax-map').innerHTML = '<div id="leaflet-map" style="height: 40em;"></div>';
       let bounds = [];
@@ -28,19 +34,19 @@
           coords.push([
             element.gps.latitude,
             element.gps.longitude,
-            0.4,
+            element.intensity,
           ]);
           bounds.push([element.gps.latitude, element.gps.longitude]);
         });
         L.heatLayer(coords, {
           minOpacity: 0.4,
           radius: 25,
-          blur: 10,
+          blur: 10
         }).addTo(map);
       }
       else {
+        let markers = L.markerClusterGroup();
         drupalSettings.leaflet.places.forEach(function(element) {
-          // Add popup with element information.
           let popup = L.popup();
           if (element.description === null) {
             popup.setContent('<a href="' + element.url + '"><h1>' + element.title + '</h1></a>');
@@ -49,13 +55,14 @@
             popup.setContent('<a href="' + element.url + '"><h1>' + element.title + '</h1><p>' + element.description + '</p></a>');
           }
           // Add marker for element.
-          L.marker([element.gps.latitude, element.gps.longitude], {
+          markers.addLayer(L.marker([element.gps.latitude, element.gps.longitude], {
             title: element.title,
             alt: element.title,
-          }).bindPopup(popup).openPopup().addTo(map);
+          }).bindPopup(popup).openPopup());
           // Add coordinates to bounding box.
           bounds.push([element.gps.latitude, element.gps.longitude]);
         });
+        map.addLayer(markers);
       }
       map.fitBounds(bounds);
     },
--- src/Form/MapForm.php
+++ src/Form/MapForm.php
@@ -4,6 +4,7 @@ namespace Drupal\effective_activism\Form;
 
 use Drupal;
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\InsertCommand;
 use Drupal\Core\Ajax\SettingsCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -32,6 +33,8 @@ class MapForm extends FormBase {
     'heatmap' => 'Heatmap',
   ];
 
+  const HEATMAP_DEFAULT_INTENSITY = 0.4;
+
   /**
    * {@inheritdoc}
    */
@@ -51,7 +54,15 @@ class MapForm extends FormBase {
       $available_filters[$filter_id] = $filter->getName();
     }
     // Get filter default value.
-    $selected_filter = !empty($form_state->getValue('filter')) ? (Filter::load($form_state->getValue('filter')))->id() : key($available_filters);
+    $selected_filter = $form_state->getValue('filter', key($available_filters));
+    // Get available data types.
+    $data_bundles = Drupal::entityManager()->getBundleInfo('data');
+    $available_datatypes = [
+      '_none' => $this->t('None'),
+    ];
+    foreach ($data_bundles as $bundle_name => $bundle_info) {
+      $available_datatypes[$bundle_name] = $bundle_info['label'];
+    }
     $form['#theme'] = (new ReflectionClass($this))->getShortName();
     $form['filter'] = [
       '#type' => 'select',
@@ -69,6 +80,12 @@ class MapForm extends FormBase {
       '#options' => self::MAP_TYPE_OPTIONS,
       '#required' => TRUE,
     ];
+    $form['data_type'] = [
+      '#default_value' => '_none',
+      '#title' => $this->t('Data type'),
+      '#type' => 'select',
+      '#options' => $available_datatypes,
+    ];
     $form['map'] = [
       '#type' => 'markup',
       '#children' => '<div id="leaflet-map" style="height: 40em;"></div>',
@@ -78,12 +95,14 @@ class MapForm extends FormBase {
         'library' => [
           'effective_activism/leaflet',
           'effective_activism/leaflet-heatmap',
+          'effective_activism/leaflet-markercluster',
         ],
         'drupalSettings' => [
           'leaflet' => [
             'key' => Drupal::config('effective_activism.settings')->get('mapbox_api_key'),
-            'places' => $this->getPlaces(Filter::load($selected_filter), $group),
+            'places' => $this->getPlaces(Filter::load($selected_filter), $group, $form_state->getValue('data_type')),
             'type' => $form_state->getValue('map_type'),
+            'data_type' => $form_state->getValue('data_type'),
           ],
         ],
       ],
@@ -118,7 +137,24 @@ class MapForm extends FormBase {
    *   Ajax response.
    */
   public function ajaxCallback(array &$form, FormStateInterface $form_state) {
-    return $form['map'];
+    $response = new AjaxResponse();
+    // Clear any previous highcharts settings.
+    $response->addCommand(new SettingsCommand([
+      'leaflet' => NULL,
+    ], TRUE));
+    $places = $this->getPlaces(Filter::load($form_state->getValue('filter')), $form_state->getTemporaryValue('group'), $form_state->getValue('data_type'));
+    if (!empty($places)) {
+      $response->addCommand(new SettingsCommand([
+        'leaflet' => [
+          'key' => Drupal::config('effective_activism.settings')->get('mapbox_api_key'),
+          'places' => $places,
+          'type' => $form_state->getValue('map_type'),
+          'data_type' => $form_state->getValue('data_type'),
+        ],
+      ], TRUE));
+      $response->addCommand(new InsertCommand(NULL, $form['map']));
+    }
+    return $response;
   }
 
   /**
@@ -126,23 +162,51 @@ class MapForm extends FormBase {
    *   The filter to apply.
    * @param Group $group
    *   An optional group.
+   * @param string $data_type
+   *   An optional data type.
    *
    * @return array
    *   An array of places.
    */
-  private function getPlaces(Filter $filter, Group $group = NULL) {
+  private function getPlaces(Filter $filter, Group $group = NULL, $data_type = NULL) {
     $places = [];
     $events = empty($group) ? FilterHelper::getEvents($filter) : FilterHelper::getEventsByGroup($filter, $group);
+    $largest_intensity = 0.0;
     foreach ($events as $event) {
       // Skip group if location is not set.
       if (empty($event->location->latitude)) {
         continue;
+      }
+      // Add intensity if data type is selected.
+      $intensity = self::HEATMAP_DEFAULT_INTENSITY;
+      if (!empty($data_type) && $data_type !== '_none') {
+        $intensity = 0.0;
+        // Skip event if it doesn't have a result that uses the data type.
+        foreach ($event->results as $result) {
+          if ($result->entity->hasField(sprintf('data_%s', $data_type)) && !$result->entity->get(sprintf('data_%s', $data_type))->isEmpty()) {
+            if (in_array($data_type, [
+              'income',
+              'expense',
+            ])) {
+              $intensity += $result->entity->get(sprintf('data_%s', $data_type))->entity->get(sprintf('field_transaction'))->value;
+            }
+            else {
+              $intensity += $result->entity->get(sprintf('data_%s', $data_type))->entity->get(sprintf('field_%s', $data_type))->value;
+            }
+          }
+        }
+        // Skip if event has no matching results.
+        if (empty($intensity) || $intensity === 0.0) {
+          continue;
+        }
+        $largest_intensity = $intensity > $largest_intensity ? $intensity : $largest_intensity;
       }
       $places[] = [
         'gps' => [
           'latitude' => $event->location->latitude,
           'longitude' => $event->location->longitude,
         ],
+        'intensity' => $intensity,
         'title' => $event->title->isEmpty() ? t('Event') : $event->title->value,
         'description' => sprintf('<p>%s<br>%s</p><p>%s</p>',
             $event->location->address,
@@ -155,6 +219,12 @@ class MapForm extends FormBase {
           'event' => $event->id(),
         ]))->toString(),
       ];
+    }
+    // Make intensity relative to largest intensity.
+    if (!empty($data_type) && $data_type !== '_none' && $largest_intensity > 0.0) {
+      foreach ($places as &$place) {
+        $place['intensity'] = $place['intensity'] / $largest_intensity;
+      }
     }
     return $places;
   }
